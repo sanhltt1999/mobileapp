05998c0b89d72f497188075e703488af21c6e702
phuctrinh39@gmail.com <phuctrinh39@gmail.com>
2021-05-19 08:47:19 +0700

    update Profile ui

---
+++ app/src/main/java/com/intern/cndd/ui/login/SignUpFragment.java
@@ -0,0 +1,29 @@
+package com.intern.cndd.ui.login;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.intern.cndd.R;
+
+public class SignUpFragment extends Fragment {
+
+    public static SignUpFragment newInstance() {
+        return new SignUpFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+    }
+}
