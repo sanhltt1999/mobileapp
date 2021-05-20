30769180dfe705e5a58e8a016b68a98ecc3b1ec3
huynhquanhht <huynhquanhht@gmail.com>
2021-05-21 01:47:57 +0700

    Create Login/remember

---
+++ app/src/main/java/com/intern/cndd/ui/MainActivity.java
@@ -0,0 +1,70 @@
+package com.intern.cndd.ui;
+
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.intern.cndd.R;
+import com.intern.cndd.ui.login.LoginFragment;
+import com.intern.cndd.ui.login.SignUpFragment;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, SignUpFragment.callAnotherFragment {
+
+    ColorStateList def;
+    TextView mLoginTextView;
+    TextView mSignUpTextView;
+    TextView mSelectTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+
+        mSelectTextView = findViewById(R.id.select);
+        def = mSignUpTextView.getTextColors();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.contentMain, new LoginFragment()).commit();
+
+    }
+
+    private void init() {
+
+        mLoginTextView = findViewById(R.id.item1);
+        mSignUpTextView = findViewById(R.id.item2);
+        mLoginTextView.setOnClickListener(this);
+        mSignUpTextView.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.item1){
+            mSelectTextView.animate().x(0).setDuration(100);
+            mLoginTextView.setTextColor(Color.WHITE);
+            mSignUpTextView.setTextColor(def);
+            getSupportFragmentManager().beginTransaction().replace(R.id.contentMain, new LoginFragment()).commit();
+        } else if (view.getId() == R.id.item2){
+            mLoginTextView.setTextColor(def);
+            mSignUpTextView.setTextColor(Color.WHITE);
+            int size = mSignUpTextView.getWidth();
+            mSelectTextView.animate().x(size).setDuration(100);
+            getSupportFragmentManager().beginTransaction().replace(R.id.contentMain, new SignUpFragment()).commit();
+        }
+    }
+
+    @Override
+    public void callBack() {
+        mSelectTextView.animate().x(0).setDuration(100);
+        mLoginTextView.setTextColor(Color.WHITE);
+        mSignUpTextView.setTextColor(def);
+        getSupportFragmentManager().beginTransaction().replace(R.id.contentMain, new LoginFragment()).commit();
+    }
+}
